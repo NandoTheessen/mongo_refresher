a67ca27c887801a80157a32cb5615fbfd9e9670e
NandoTheessen <nando.theessen@gmail.com>
2019-02-13 04:57:08 +0100

    Adds database config

---
+++ src/data/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+module.exports = {
+  connectTo: function(database = 'sandbox', host = 'localhost') {
+    return mongoose.connect(`mongodb://${host}/${database}`, {
+      useNewUrlParser: true
+    });
+  }
+};
