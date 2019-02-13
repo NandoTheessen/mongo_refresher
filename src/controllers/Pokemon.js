fc91c98c4726709142305ae60cda1bedde45715f
NandoTheessen <nando.theessen@gmail.com>
2019-02-13 13:22:13 +0100

    Adds comments & formalizes responses for pokemon endpoint

--- src/controllers/Pokemon.js
+++ src/controllers/Pokemon.js
@@ -1,24 +1,27 @@
-const Pokemon = require('../data/models/Pokemon');
-const Ability = require('../data/models/Abilities');
+const Pokemon = require('../models/Pokemon');
+const Ability = require('../models/Abilities');
 const fetchData = require('../helpers/FetchData');
 const API = process.env.API || 'https://pokeapi.co/api/v2/pokemon';
 
 const getPokemon = async (req, res) => {
   try {
     const { name } = req.body;
-    const query = await Pokemon.find({ name }, { _id: 0, _v: 0 }).populate(
+    const query = await Pokemon.find({ name }, { _id: 0 }).populate(
       'abilities',
       {
-        _id: 0,
-        _v: 0
+        _id: 0
       }
     );
     let fromCache = true;
 
     if (query.length === 0) {
       fromCache = false;
+
       const apiResponse = await fetchData(`${API}/${name.toLowerCase()}`);
+
       const { abilities } = apiResponse;
+
+      // Adapt API response to fit db schema
       const newAbilities = abilities.map(ab => {
         return {
           name: ab.ability.name,
@@ -27,32 +30,26 @@ const getPokemon = async (req, res) => {
           slot: ab.slot
         };
       });
+
+      // Use ability id's to create Pokemon
       const insertedAbilities = await Ability.insertMany(newAbilities);
-      const abilityIds = insertedAbilities.map(ab => ab._id);
       await new Pokemon({
         name,
-        abilities: abilityIds
+        abilities: insertedAbilities.map(ab => ab._id)
       }).save();
-      res.status(201).json({ name, abilities: newAbilities, fromCache });
+
+      // Return new entry to the user
+      res.status(201).json({ name, abilities: insertedAbilities, fromCache });
     } else {
-      res.status(200).json({ data: query, fromCache });
+      // Return saved resource to the user
+      const { abilities, name } = query[0];
+      res.status(200).json({ name, abilities, fromCache });
     }
   } catch (e) {
-    res.status(500).json({ msg: 'nope', error: e.response });
+    res
+      .status(500)
+      .json({ msg: 'Server encountered an error processing your request' });
   }
 };
 
-const savePokemon = async (req, res) => {
-  console.log(req.body);
-  const { name } = req.body;
-  const newPokemon = new Pokemon({ name });
-  try {
-    const newP = await newPokemon.save();
-    console.log(newP);
-    res.status(201).json({ data: newP });
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-module.exports = { getPokemon, savePokemon };
+module.exports = { getPokemon };
